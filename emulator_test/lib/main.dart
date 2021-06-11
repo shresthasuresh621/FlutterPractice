fca2d3a7dc3c998f41cdbd075a8f0d833e819125
FlutterPractice <shresthasuresh621@gmail.com>
2021-06-11 13:22:36 +0545

    Initial Changes

---
+++ emulator_test/lib/main.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+void main(){
+  runApp(
+      Center(
+        child: Text(
+        "This is the example of First app",
+        textDirection: TextDirection.ltr
+      ),
+    )
+  );
+}
